663db1f6a0fe41c517d937818169259b46e2a729
bahrombekboymurodov <baxrombekboymurodov@gmail.com>
2025-06-16 00:33:21 +0500

    km dan metrga o'tish added

---
+++ km dan metr ga o'tish .py
@@ -0,0 +1,15 @@
+#Create a program that converts kilometers to meters and centimeters.
+print("A program that converts kilometers to meters and centimeters")
+
+km = float(input ("200: "))
+
+
+
+
+meter=km*1000
+centimeter=km*100000
+
+print(f"{km} kilometer={meter} meter ")
+
+print(f"{km} kilometer={centimeter} centimeter")
+
