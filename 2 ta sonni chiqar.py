6798e75b0a4f27318d862767f4611bd6a883c8bb
bahrombekboymurodov <baxrombekboymurodov@gmail.com>
2025-06-16 00:46:52 +0500

    2 ta sonni chiqar.py added

---
+++ 2 ta sonni chiqar.py
@@ -0,0 +1,17 @@
+#Write a program that takes two numbers and prints out the result of integer division and theremainder.
+
+print("Enter two numbers to divide:")
+
+
+a = int(input("33"))
+b = int(input("11 "))
+
+
+
+quotient = a // b      
+remainder = a % b      
+
+
+print(f" Integer division result: {a} // {b} = {quotient}")
+print(f"remainder: {a} % {b} = {remainder}")
+
