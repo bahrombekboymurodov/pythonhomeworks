4751ae673916922e048bf1bc4a4d90ddf569afc1
bahrombekboymurodov <baxrombekboymurodov@gmail.com>
2025-06-16 00:16:40 +0500

    second question

---
+++ number data type ques.py
@@ -0,0 +1,22 @@
+#Write a Python file that asks for three numbers and outputs the largest and smallest.
+
+def main():
+    print("1029384 1929238 1023838:")
+a=float(input("1029384"))
+b=float(input("1929238"))
+c=float(input("1023838"))
+
+
+
+largest=max(a,b,c,)
+smallest=min(a,b,c,)
+
+print(f"largest is:{largest}")
+print(f"smallest is:{smallest}")
+
+
+
+
+
+
+
