ba874d55f1d108aeab162bef4ffa9cafc6222106
bahrombekboymurodov <baxrombekboymurodov@gmail.com>
2025-06-18 01:29:08 +0500

    boolean1 and boolean2 added

---
+++ boolean1.py
@@ -0,0 +1,14 @@
+#Write a program that accepts a username and password and checks if both are not empty
+
+
+username=input("akamakasi:")
+password=input("7777:")
+
+if username != "" and password != "":
+
+  print ("the username and password accepted.")
+
+else:
+
+    print ("the username and password can't be empty!")
+
---
+++ boolean2.py
@@ -0,0 +1,9 @@
+#Create a program that checks if two numbers are equal and outputs a message if they are.
+
+number1 = float(input("the first number:4567"))
+number2 = float(input("the second number:6789"))
+
+
+
+if number1==number2:
+ print("the numbers  are equal")
