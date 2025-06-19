d9026b303d6afebc9d7ad0b630b40e99a219f38e
bahrombekboymurodov <baxrombekboymurodov@gmail.com>
2025-06-19 23:05:25 +0500

    lesson-2 added bu lesson-1 emas

---
+++ lesson-1/lesson-2/lesson-2/string.py
@@ -0,0 +1,5 @@
+#Create a program that takes a float number as input and rounds it to 2 deci
+a=int(input("5.498349"))
+
+print(a*2)
+
---
+++ lesson-1/lesson-2/nimadir.py
@@ -0,0 +1,14 @@
+#Create a program that takes a number and checks if it’s even or not.
+
+number=int(input("38290: "))
+
+if number % 2 == 0:
+
+
+
+print("The number is even")
+
+else:
+    print("the number is not even")
+
+
